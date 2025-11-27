9f78e9473a4de57134748e70d4477e34d3073a02
Ifeoluwa Sulaiman <ifeoluwasulaiman30@gmail.com>
2025-11-27 06:50:45 +0100

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+
+
+    // {
+    //     path: '**',
+    //     redirectTo: 'login',
+    // },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { Injectable, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+
+@Injectable({
+    providedIn: "root",
+})
+
+@NgModule({
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+
+    ],
+    providers: [provideAnimationsAsync()],
+
+    bootstrap: [AppComponent],
+})
+export class AppModule { }
